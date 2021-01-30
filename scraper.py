3a657e6e2543bef07c449f079f833f579e574886
Codey Huntting <creativecodey@gmail.com>
2021-01-29 19:51:13 -0800

    Some refactoring of the beautiful soup and fixed an error

--- scraper.py
+++ scraper.py
@@ -5,39 +5,22 @@ from bs4 import BeautifulSoup, SoupStrainer
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    return [link for link in links if is_valid(link, resp)]
 
 
 def extract_next_links(url, resp):
-    # TODO: Implement function to extract the next link.
-
     # List of links to return
     links = list()
 
-    # Check the status integer
-    #   status 200-399: OK, use the html retrieved in resp.raw_response.content to return list of links
-    #   status 400-599: HTTP ERROR, use resp.raw_response to report the error and return empty list
-    #   status 600-609: CACHING ERROR, use resp.error to report the error and return empty list
-
     # Response status OK, retrieve the links and add them
     if 200 <= resp.status < 400:
-        # TODO: get the links from the http response and add them to the list
+        soup = BeautifulSoup(resp.raw_response.content, parser='html.parser')
 
-        # PSEUDOCODE
-        # Parse resp.raw_response.content for the links in the webpage
-        #   USE BeautifulSoup
-        for link in BeautifulSoup(resp.raw_response.content, parse_only=SoupStrainer('a')):
+        # TODO: check if this page has good information content
+
+        for link in soup.find_all('a'):
             if link.has_attr('href'):
-
-                href = link['href']
-                # href = urlparse(link['href'])
-                #
-                # # If netloc is false, the href is relative: add the href to the end of the url
-                # if not href.netloc:
-                #     href = url + "/" + href
-
-                if is_valid(href, resp):
-                    links.append(href)
+                links.append(link.get('href'))
 
     # A standard http error has occurred
     elif 400 <= resp.status < 600:
@@ -73,7 +56,7 @@ def is_valid(url, resp):
                 "today.uci.edu/department/information_computer_sciences" not in url:
             return False
 
-        # Check if the url is too similar to the response content
+        # TODO: Check if the html at the url is too similar to the response content
 
         # Remove non-webpage links
         return not re.match(
